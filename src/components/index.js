0867a700414e387862bb1a704a8c0521236747a8
anamk182006-code <anamk182006@gmail.com>
2026-07-03 16:49:41 +0530

    Part 6g: Create Index File to Export All Components

---
+++ src/components/index.js
@@ -0,0 +1,50 @@
+// Button Components
+export { Button, IconButton, ButtonGroup, LinkButton } from './Button'
+
+// Card and Form Components
+export {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+  CardFooter,
+  Form,
+  FormGroup,
+  FormLabel,
+  FormInput,
+  FormTextarea,
+  FormSelect,
+  FormCheckbox,
+  FormRadio,
+  FormError,
+  FormSuccess,
+  FormHelperText,
+} from './CardForm'
+
+// Modal, Dialog, and Drawer Components
+export {
+  Modal,
+  useModal,
+  Dialog,
+  useDialog,
+  Drawer,
+  useDrawer,
+  Tooltip,
+  Popover,
+} from './Modal'
+
+// Alert and Notification Components
+export {
+  Alert,
+  Toast,
+  useToast,
+  ToastContainer,
+  Notification,
+  Badge,
+  Banner,
+  Callout,
+} from './Alert'
+
+// Additional Hooks and Utilities
+export { useModal, useDialog, useDrawer, useToast } from './Modal'
